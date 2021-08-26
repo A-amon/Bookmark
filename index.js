65e2ea93bda2d94954c376366427a041a52edf32
BHD-Ilyes <ilyes.hajdahman@rades.r-iset>
2021-08-26 23:07:07 +0100

    update css

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-function validation() {
+function validation(event) {
     var form = document.getElementById("form");
+    event.preventDefault();
     var email = document.getElementById("email").value;
     var text = document.getElementById("text");
     var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-
+    var form_input = document.querySelector('.form-input')
     if (! email.match(pattern)) {
         text.innerHTML = "Whoops, make sure it's an email";
-        form.style.background= "url(/assets/icons/icon-error.svg)";
-        form.style.backgroundRepeat = "no-repeat";
-        
-        text.style.backgroundColor="hsl(0, 94%, 66%)";
-
-    } 
+        form_input.classList.add('error');
+} 
 }
