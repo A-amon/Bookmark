dbb4e1815d76db295cdb8100c9b12fed409eb7e8
simretB05 <happysimret@gmai.com>
2021-08-27 15:35:02 -0600

    navbar updat on click color chnage

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const bars = document.querySelector(".bars")
 const dropDown = document.querySelector(".sb1")
 const close = document.querySelector(".close")
+const color = document.querySelector(".color-change")
+const navbar = document.querySelector(".navbar")
 
 /*
 * Show/ hide nav dropdown
@@ -14,7 +16,7 @@ bars.addEventListener("click", function () {
     dropDown.classList.toggle("change")
     bars.classList.remove("show")
     close.classList.add("show")
-    close.classList.remove("color-change")
+
 })
 
 /*
@@ -25,4 +27,13 @@ close.addEventListener("click", function () {
     close.classList.remove("show")
     bars.classList.add("show")
     bars.classList.remove("color-change")
+})
+
+bars.addEventListener("click", function () {
+
+    navbar.classList.add("color-change")
+})
+
+close.addEventListener("click", function () {
+    navbar.classList.remove("color-change")
 })
