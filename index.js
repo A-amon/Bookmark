c88b1b6a43ab4699982c4dfe9b17aead5df94813
A-amon <64846892+A-amon@users.noreply.github.com>
2021-09-03 14:55:21 +0800

    Fix tab navigation via key pressed

--- index.js
+++ index.js
@@ -19,8 +19,12 @@ const setStaggerAnimation = () => {
     const toValues = {
         opacity: 1,
         y: (index) => {
+            //  If laptop size
+            //  Display in declining pattern
             if (currentWidth >= 1020)
                 return index * 50
+
+            // Else, keeps all cards in same row
             return 0
         },
         stagger: {
@@ -67,7 +71,7 @@ const setResizeListener = () => {
          */
         resizeTimeout = setTimeout(() => {
             clearResizeTimeout()
-            setStaggerAnimation()
+            setStaggerAnimation()   // Play animation // Re-position cards
         }, 2000)
     })
 }
@@ -101,39 +105,52 @@ const handleTabClick = (event) => {
     showPanel(panelId)
 }
 
+var prevTabInd  // Update current tab position when tab is navigated using keyboard
+
 /**
  * Handle tab key pressed event
  * Allows navigation of tabs via arrow key
+ * E.g. 
+ * Press right arrow key, moves focus to next tab
+ * Press left arrow key, moves focus to prev tab
  * @param  {object} event
- * @param  {number} currentInd
  */
-const handleTabKey = (event, currentInd) => {
+const handleTabKey = (event) => {
     const key = event.keyCode
     const tabsArray = Array.from(tabs)
     const tabsLength = tabsArray.length
 
-    let nextInd = currentInd
+    let nextInd = prevTabInd
 
     switch (key) {
         // Left arrow
         case 37:
-            nextInd--
+            nextInd--   // move to left by 1
             break
         // Right arrow
         case 39:
-            nextInd++
+            nextInd++   // move to right by 1
             break
         default:
             break
     }
 
+    /**
+     * Keeps focus within tabs range
+     * Prevent out of bounds 
+     */
+
+    // Set nextInd to first tab if exceeds tabs length/size
     if (nextInd > tabsLength - 1)
         nextInd = 0
+    //  Set nextInd to last tab if negative index
     else if (nextInd < 0)
         nextInd = tabsLength - 1
 
     // Only focus on tab if nextInd changes
-    nextInd !== currentInd && tabsArray[nextInd].focus()
+    nextInd !== prevTabInd && tabsArray[nextInd].focus()
+
+    prevTabInd = nextInd
 }
 
 /**
@@ -155,9 +172,18 @@ const showPanel = (id) => {
     const panel = document.querySelector(`#${id}`)
     panel.removeAttribute('hidden')
     panel.focus()
+}
 
-    const panelInd = Array.from(panels).indexOf(panel)
-    panel.addEventListener('keyup', event => handleTabKey(event, panelInd))
+/**
+ * Add keyup listener to .features section
+ */
+const addKeyListenerToTab = () => {
+    const currentTab = document.querySelector('.features__tab[aria-selected="true"]')
+    const tabInd = Array.from(tabs).indexOf(currentTab)
+    const featuresSection = document.querySelector('.features')
+
+    prevTabInd = tabInd // initialize prevTabInd
+    featuresSection.addEventListener('keyup', event => handleTabKey(event))
 }
 
 /**
@@ -181,6 +207,7 @@ const selectTab = (tab) => {
     tab.setAttribute('tab-index', 0)
 }
 
+addKeyListenerToTab()
 for (let tab of tabs) {
     tab.addEventListener('click', handleTabClick)
 }
