1873d53c5b067324389f5d5fc4ab1a8d4d5bea6e
simretB05 <happysimret@gmai.com>
2021-09-03 13:25:56 -0600

    fixed popup-appear

--- index.js
+++ index.js
@@ -292,20 +292,17 @@ function displayQuote(response) {
 let apiUrl = " https://quote-garden.herokuapp.com/api/v3/quotes/random";
 axios.get(apiUrl).then(displayQuote);
 
+// email input validation
 function validation(event) {
-    // email input validation
-    function validation(event) {
-        var form = document.getElementById("form");
-        event.preventDefault();
-        var email = document.getElementById("email").value;
-        var text = document.getElementById("text");
-        var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-        var form_input = document.querySelector('.form-input')
-        if (!email.match(pattern)) {
-            text.innerHTML = "Whoops, make sure it's an email";
-            form_input.classList.add('error');
-        }
-        form.reset();
+    var form = document.getElementById("form");
+    event.preventDefault();
+    var email = document.getElementById("email").value;
+    var text = document.getElementById("text");
+    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    var form_input = document.querySelector('.form-input')
+    if (!email.match(pattern)) {
+        text.innerHTML = "Whoops, make sure it's an email";
+        form_input.classList.add('error');
     }
-
-
+    form.reset();
+}
